63468b4b621a6c3cb88d3a61e0d80aafff1d11e8
Denis Tsvettsikh <denis_tsvettsikh@epam.com>
2020-11-26 12:05:19 +0700

    Fix mapping

--- src/CleanArchitecture.Web/Controllers/ToDoController.cs
+++ src/CleanArchitecture.Web/Controllers/ToDoController.cs
@@ -1,26 +1,32 @@
-﻿using CleanArchitecture.Core;
+﻿using System.Collections.Generic;
+using CleanArchitecture.Core;
 using CleanArchitecture.Core.Entities;
 using CleanArchitecture.SharedKernel.Interfaces;
 using CleanArchitecture.Web.ApiModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using CleanArchitecture.Web.Endpoints.ToDoItems;
 
 namespace CleanArchitecture.Web.Controllers
 {
     public class ToDoController : Controller
     {
         private readonly IRepository _repository;
+        private readonly IMapper _mapper;
 
-        public ToDoController(IRepository repository)
+        public ToDoController(IRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
         {
-            var items = (await _repository.ListAsync<ToDoItem>())
-                            .Select(ToDoItemDTO.FromToDoItem);
+            var entities = await _repository.ListAsync<ToDoItem>();
+            var items = _mapper.Map<List<ToDoItemDTO>>(entities);
+
             return View(items);
         }
 
